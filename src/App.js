64c659e4001713d6e64e83b533f2c1f17395f8f6
Idan Lev <idan.93lev@gmail.com>
2022-01-25 17:51:22 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello from Eric</div>;
+  return <div className="App">my name is slim tuki</div>;
 }
 
 export default App;
